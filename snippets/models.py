fc3dbcbe958346cdf97a275b612dcd0c4cd61a8a
Takeshi Itami <tksh.itm@gmail.com>
2019-05-03 10:54:41 +0900

    format

--- snippets/models.py
+++ snippets/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
-from pygments.lexers import (get_all_lexers,
-                             get_lexer_by_name)
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -11,29 +10,21 @@ STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 
 class Snippet(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
+    title = models.CharField(max_length=100, blank=True, default="")
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(
-        choices=LANGUAGE_CHOICES,
-        default='python',
-        max_length=100
+        choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
-    style = models.CharField(
-        choices=STYLE_CHOICES,
-        default='friendly',
-        max_length=100
-    )
+    style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
     owner = models.ForeignKey(
-        'auth.User',
-        related_name='snippets',
-        on_delete=models.CASCADE
+        "auth.User", related_name="snippets", on_delete=models.CASCADE
     )
     highlighted = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ("created",)
 
     def save(self, *args, **kwargs):
         """
@@ -41,9 +32,10 @@ class Snippet(models.Model):
         representation of the code snippet.
         """
         lexer = get_lexer_by_name(self.language)
-        linenos = 'table' if self.linenos else False
-        options = {'title': self.title} if self.title else {}
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
         formatter = HtmlFormatter(
-            style=self.style, linenos=linenos, full=True, **options)
+            style=self.style, linenos=linenos, full=True, **options
+        )
         self.highlighted = highlight(self.code, lexer, formatter)
         super(Snippet, self).save(*args, **kwargs)
--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from snippets import views
 from snippets.views import SnippetDetail, SnippetList
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path("snippets/", views.SnippetList.as_view()),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
